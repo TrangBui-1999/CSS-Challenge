3c70f64a18326d311172dd69d062583aa406e119
Trang Bui <trangbui671@gmail.com>
2023-06-30 23:21:58 +0700

    Fix responsive web

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function openNav() {
-  document.getElementById("openMenu").style.width = "25%";
+  document.getElementById("openMenu").style.width = "370px";
+  document.getElementById("openMenu").style.height = "100%";
 }
 
 function closeNav() {
   document.getElementById("openMenu").style.width = "0";
 }
+window.scroll({
+  behavior: "smooth",
+});
