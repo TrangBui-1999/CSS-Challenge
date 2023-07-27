1cb39fa42c050961bb6503da7ffd0d2525447e14
Trang Bui <trangbui671@gmail.com>
2023-07-28 03:00:20 +0700

    Fix mobile version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function openNav() {
   document.getElementById("openMenu").style.width = "370px";
-  document.getElementById("openMenu").style.height = "100%";
+  document.getElementById("openMenu").style.height = "100vh";
 }
 
 function closeNav() {
@@ -9,3 +9,20 @@ function closeNav() {
 window.scroll({
   behavior: "smooth",
 });
+
+const toggle = document.querySelector(".toggle");
+const nav = document.querySelector(".nav-row");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    nav.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
