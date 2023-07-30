a1563c2a33fe992d7e8f863e2f9b2485cdecf57d
Trang Bui <trangbui671@gmail.com>
2023-07-31 00:26:37 +0700

    New layout for mobile

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function openNav() {
   document.getElementById("openMenu").style.width = "370px";
   document.getElementById("openMenu").style.height = "100vh";
+  document.getElementById("openMenu").style.border = "solid white 5px";
 }
 
 function closeNav() {
   document.getElementById("openMenu").style.width = "0";
+  document.getElementById("openMenu").style.border = "none";
 }
 window.scroll({
   behavior: "smooth",
 });
 
 const toggle = document.querySelector(".toggle");
+console.log(toggle);
 const nav = document.querySelector(".nav-row");
 const items = document.querySelectorAll(".item");
 
@@ -26,3 +29,82 @@ function toggleMenu() {
 }
 /* Event Listeners */
 toggle.addEventListener("click", toggleMenu, false);
+
+/* Mobile utils*/
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowIcon = document.querySelectorAll(".wrapper i");
+const carouselCard = [...carousel.children];
+console.log(carouselCard);
+let isAutoPlay = true,
+  isDragging = false,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+arrowIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    carousel.scrollLeft += icon.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+const dragStart = (e) => {
+  e.preventDefault();
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+const dragging = (e) => {
+  if (!isDragging) return;
+
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+const dragStop = (e) => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  } else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
+const autoPlay = () => {
+  if (window.innerWidth < 500 || !isAutoPlay) return;
+  timeoutId = setTimeout(
+    () => (carousel.scrollLeft += firstCardWidth),
+
+    2500
+  );
+};
+autoPlay();
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+/* Change html base on screen */
+if (window.innerWidth > 480) {
+  $(".wrapper").contents().unwrap();
+  $(".carousel").contents().unwrap();
+}
